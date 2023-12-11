aad3bf13263269fd2c941cae0a6c9712c094838a
Mario Reder <mario.reder@pm.me>
2023-12-11 13:47:45 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { Theme } from '@radix-ui/themes';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Theme>
+      <App />
+    </Theme>
+  </React.StrictMode>
+);
