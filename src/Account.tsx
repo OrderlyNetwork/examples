82cf451b9092556c1a5075e1a631ecbc54f0d495
Mario Reder <mario.reder@pm.me>
2023-12-13 09:52:59 +0100

    account info

--- src/Account.tsx
+++ src/Account.tsx
@@ -1,6 +1,6 @@
 import { useAccount } from '@orderly.network/hooks';
 import { AccountStatusEnum } from '@orderly.network/types';
-import { Button, Flex, Heading } from '@radix-ui/themes';
+import { Button, Card, Container, Flex, Heading, Text } from '@radix-ui/themes';
 import { JsonRpcSigner } from 'ethers';
 import { FC, useEffect } from 'react';
 
@@ -19,12 +19,51 @@ export const Account: FC<{ signer?: JsonRpcSigner }> = ({ signer }) => {
     });
   }, [signer, account]);
 
+  console.log('state', state);
+
   return (
     <Flex style={{ margin: '1.5rem' }} gap="3" align="center" justify="center" direction="column">
       <Heading>Account</Heading>
 
+      <Card style={{ maxWidth: 240 }}>
+        {state.accountId ? (
+          <>
+            <Flex gap="2" direction="column">
+              <Container>
+                <Text as="div" size="2" weight="bold">
+                  Orderly Account ID:
+                </Text>
+                <Text as="div" size="2">
+                  {state.accountId}
+                </Text>
+              </Container>
+              <Container>
+                <Text as="div" size="2" weight="bold">
+                  Address:
+                </Text>
+                <Text as="div" size="2">
+                  {state.address}
+                </Text>
+              </Container>
+              <Container>
+                <Text as="div" size="2" weight="bold">
+                  User ID:
+                </Text>
+                <Text as="div" size="2">
+                  {state.userId}
+                </Text>
+              </Container>
+            </Flex>
+          </>
+        ) : (
+          <Text as="div" size="3" weight="bold" color="red">
+            Not connected!
+          </Text>
+        )}
+      </Card>
+
       <Button
-        disabled={state.status > AccountStatusEnum.NotSignedIn}
+        disabled={state.status !== AccountStatusEnum.Connected}
         onClick={() => {
           account.createAccount();
         }}
@@ -33,7 +72,10 @@ export const Account: FC<{ signer?: JsonRpcSigner }> = ({ signer }) => {
       </Button>
 
       <Button
-        disabled={state.status > AccountStatusEnum.DisabledTrading}
+        disabled={
+          state.status > AccountStatusEnum.DisabledTrading ||
+          state.status === AccountStatusEnum.NotConnected
+        }
         onClick={() => {
           account.createOrderlyKey(30);
         }}
